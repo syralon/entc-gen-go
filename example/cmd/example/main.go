61f87bc86246eb9e59c05bf9f8ee7f6c616dad6a
senn <syralonor@gmail.com>
2025-11-28 18:07:00 +0800

    update

--- example/cmd/example/main.go
+++ example/cmd/example/main.go
@@ -1,25 +1,102 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"github.com/syralon/entc-gen-go/example/ent"
-	"github.com/syralon/entc-gen-go/example/internal/service"
-	pb "github.com/syralon/entc-gen-go/example/proto/example"
-	"net/http"
+	"flag"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+
+
+	"github.com/syralon/entc-gen-go/example/internal/conf"
 )
 
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Name is the name of the compiled software.
+	Name string
+	// Version is the version of the compiled software.
+	Version string
+	// flagconf is the config flag.
+	flagconf string
+)
+
+func init() {
+	flag.StringVar(&flagconf, "conf", "config.yaml", "config path, eg: -conf config.yaml")
+}
+
+func newApp(hs *http.Server, gs *grpc.Server) *kratos.App {
+	return kratos.New(
+		kratos.Name(Name),
+		kratos.Version(Version),
+		kratos.Metadata(map[string]string{}),
+		//kratos.Logger(logger),
+		kratos.Server(
+			hs,
+			gs,
+		),
+	)
+}
+
+// Set global trace provider
+func setTracerProvider(url string) error {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Set the sampling rate based on the parent span to 100%
+		tracesdk.WithSampler(tracesdk.ParentBased(tracesdk.TraceIDRatioBased(1.0))),
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in an Resource.
+		tracesdk.WithResource(resource.NewSchemaless(
+			semconv.ServiceNameKey.String(Name),
+			attribute.String("env", "dev"),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+	return nil
+}
+
 func main() {
-	ctx := context.Background()
-	client := ent.NewClient()
+	flag.Parse()
 
-	mux := runtime.NewServeMux()
+	cfg := config.New(
+		config.WithSource(
+			file.NewSource(flagconf),
+		),
+	)
+	if err := cfg.Load(); err != nil {
+		panic(err)
+	}
 
-	_ = pb.RegisterGroupServiceHandlerServer(ctx, mux, service.NewGroupService(client))
-	_ = pb.RegisterUserServiceHandlerServer(ctx, mux, service.NewUserService(client))
+	var bc conf.Bootstrap
+	if err := cfg.Scan(&bc); err != nil {
+		panic(err)
+	}
 
-	if err := http.ListenAndServe("0.0.0.0:0", mux); err != nil {
-		fmt.Println(err)
+	if err := setTracerProvider(bc.Trace.Endpoint); err != nil {
+		panic(err)
+	}
+
+	app, cleanup, err := initApp(bc.Server, bc.Data)
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
+
+	// start and wait for stop signal
+	if err := app.Run(); err != nil {
+		panic(err)
 	}
 }
