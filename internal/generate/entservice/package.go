a4bd7db681408ba9747ced30c0cf5350e1f28a8d
senn <syralonor@gmail.com>
2025-12-02 18:58:35 +0800

    controller

--- internal/generate/entservice/package.go
+++ internal/generate/entservice/package.go
@@ -4,4 +4,5 @@ const (
 	pkgEntproto = "github.com/syralon/entc-gen-go/proto/syralon/entproto"
 	entsql      = "entgo.io/ent/dialect/sql"
 	timestamppb = "google.golang.org/protobuf/types/known/timestamppb"
+	pkgWire     = "github.com/google/wire"
 )
